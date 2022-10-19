0f7676dc760b4febd05a03d50dc34ae0a35a15f6
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-10-19 11:15:56 +0000

    reactive navbar!

--- resources/js/Components/forms/Searchbar.vue
+++ resources/js/Components/forms/Searchbar.vue
@@ -2,21 +2,21 @@
 import { MagnifyingGlassIcon } from "@heroicons/vue/24/solid";
 
 export default {
-  components: { MagnifyingGlassIcon },
+    components: { MagnifyingGlassIcon },
 };
 </script>
 
 <template>
-  <div
-    class="ml-8 flex items-center justify-center rounded-md border bg-gray-200 pl-2 p-1"
-  >
-    <label for="searchinput">
-      <MagnifyingGlassIcon class="h-5 w-5 text-gray-500" />
-    </label>
-    <input
-      id="searchinput"
-      class="ml-2 mr-1 w-36 border-none bg-gray-200 text-xs text-gray-500 outline-none focus:w-48 transform duration-150 forwards"
-      placeholder="Suchen..."
-    />
-  </div>
+    <div
+        class="ml-8 inline-flex items-center justify-center rounded-md border bg-gray-200 p-1 pl-2"
+    >
+        <label for="searchinput">
+            <MagnifyingGlassIcon class="h-5 w-5 text-gray-500" />
+        </label>
+        <input
+            id="searchinput"
+            class="ml-2 mr-1 w-36 border-none bg-gray-200 text-xs text-gray-500 outline-none duration-150 focus:w-48 o-md:w-24"
+            placeholder="Suchen..."
+        />
+    </div>
 </template>
