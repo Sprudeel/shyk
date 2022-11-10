6b922404dcf8f938054a83a4cd696163f5ea6a6b
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-10 21:47:01 +0000

    🚑 add MyProfile href link to UserDropdown

--- resources/js/Components/NavBarUserSettings.vue
+++ resources/js/Components/NavBarUserSettings.vue
@@ -39,23 +39,25 @@ const user = computed(() => usePage().props.value.auth.user);
                     class="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 >
                     <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-blue-300 font-semibold text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                            >
-                                <UserIcon
-                                    :active="active"
-                                    class="shyk-blue mr-2 h-5 w-5"
-                                    aria-hidden="true"
-                                />
-                                Mein Profil
-                            </button>
-                        </MenuItem>
+                        <Link :href="'/user/' + user.username">
+                            <MenuItem v-slot="{ active }">
+                                <button
+                                    :class="[
+                                        active
+                                            ? 'bg-blue-300 font-semibold text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    <UserIcon
+                                        :active="active"
+                                        class="shyk-blue mr-2 h-5 w-5"
+                                        aria-hidden="true"
+                                    />
+                                    Mein Profil
+                                </button>
+                            </MenuItem>
+                        </Link>
                         <MenuItem v-slot="{ active }">
                             <button
                                 :class="[
