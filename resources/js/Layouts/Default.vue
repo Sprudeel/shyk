7ed0d1fa83a5d9ffea96258fb786ce7f040a1021
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-17 16:18:20 +0000

    ✨ add flash messages to layout

--- resources/js/Layouts/Default.vue
+++ resources/js/Layouts/Default.vue
@@ -1,10 +1,13 @@
 <script setup>
 import NavBar from "@/Components/Navbar.vue";
 import Footer from "@/Components/Footer.vue";
+import FlashMessages from "@/Components/FlashMessages.vue";
 </script>
 
 <template>
     <NavBar />
+
+    <FlashMessages />
 
     <slot />
 
