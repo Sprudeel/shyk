aeefd99a2bdb2efa58e53f86530100f5149bd2e4
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-19 15:46:20 +0000

    ➕ add vfm to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
+import { vfmPlugin } from "vue-final-modal";
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -21,6 +22,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(vfmPlugin)
             .mount(el);
     },
 });
