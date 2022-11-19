76f46e29edfbc25665a9774243b618dd5d56757a
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-19 12:33:06 +0000

    💄 update FlashMessages design

--- resources/js/Components/FlashMessages.vue
+++ resources/js/Components/FlashMessages.vue
@@ -13,17 +13,17 @@ const flash = computed(() => usePage().props.value.flash);
 <template>
     <span
         v-if="flash.success"
-        class="sticky top-24 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-green-300 py-4 px-6 text-green-800"
+        class="absolute top-12 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-green-200 py-4 px-6 text-green-800"
         ><CheckIcon class="mr-4 w-4" /> {{ flash.success }}
     </span>
     <span
         v-if="flash.notification"
-        class="sticky top-24 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-blue-300 p-4 text-blue-800"
+        class="absolute top-12 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-blue-200 p-4 text-blue-800"
         ><InformationCircleIcon class="mr-4 w-4" /> {{ flash.notification }}
     </span>
     <span
         v-if="flash.error"
-        class="sticky top-24 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-red-300 p-4 text-red-800"
+        class="absolute top-12 right-12 float-right flex w-fit animate-flash flex-row rounded-lg bg-red-200 p-4 text-red-800"
         ><XMarkIcon class="mr-4 w-4" /> {{ flash.error }}
     </span>
 </template>
