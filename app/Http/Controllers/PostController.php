6efa76facc01dc9d711ed7cdebbfd156c6bb4a45
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-21 21:45:32 +0000

    ✨ add Store Function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,8 @@ use Spatie\QueryBuilder\QueryBuilder;
 use Illuminate\Http\RedirectResponse;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -24,5 +26,36 @@ class PostController extends Controller
             'topics' => Topic::all(),
             'categories' => Category::all(),
         ]);
+    }
+
+    public function store(Request $request) {
+        $this->authorize('create', Post::class);
+
+
+        $request->validate([
+            'title' => 'required|unique:App\Models\Post,title',
+            'topic' => 'required|exists:topics,slug',
+            'status' => 'required',
+            'category' => 'required|exists:categories,slug',
+            'content' => 'required',
+            'file' => 'max:2048',
+        ]);
+
+        $author = Auth::user()->id;
+
+        $post = Post::create([
+            'title' => $request->title,
+            'topic' => $request->topic,
+            'status' => $request->status,
+            'category' => $request->category,
+            'content' => $request->content,
+            'author' => $author,
+        ]);
+
+        if($request->hasFile('file') && $request->validate(['file' => 'mimes:png,jpg,jpeg,pdf'])) {
+            $file = $request->file->storeAs($post->id, $request->file->getClientOriginalName());
+        }
+
+        return redirect(route('discover'))->with('success', 'Post wurde erstellt!');
     }
 }
