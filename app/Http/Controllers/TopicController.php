ac8dbaffb3b13b6237b143e6cdb03be2d01e1ce5
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-21 08:38:59 +0000

    ✨ add all topics and categories to get request

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topic;
+use App\Models\Category;
 use Illuminate\Support\Collection;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
@@ -14,6 +15,10 @@ use Illuminate\Support\Str;
 class TopicController extends Controller
 {
     public function index(Request $request) {
-        return Inertia::render('Discover', ['topic' => $request->topic ? Topic::where('slug', $request->topic)->firstOrFail() : null ]);
+        return Inertia::render('Discover', [
+            'topic' => $request->topic ? Topic::where('slug', $request->topic)->firstOrFail() : null ,
+            'topics' => Topic::all(),
+            'categories' => Category::all(),
+        ]);
     }
 }
