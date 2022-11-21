325fa949be7f1fbbcc2e139972a2d58d9679ae72
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-21 09:31:49 +0000

    ✨ add fillable var and create method to PostContr

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Collection;
+use Inertia\Inertia;
+use ProtoneMedia\LaravelQueryBuilderInertiaJs\InertiaTable;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Http\RedirectResponse;
+use App\Models\Topic;
+use App\Models\Category;
 
 class PostController extends Controller
 {
-    //
+    protected $fillable = ['title', 'author', 'subject', 'status', 'content'];
+
+
+    public function create() {
+        $this->authorize('create', Post::class);
+
+        return Inertia::render('Post/Create', [
+            'topics' => Topic::all(),
+            'categories' => Category::all(),
+        ]);
+    }
 }
