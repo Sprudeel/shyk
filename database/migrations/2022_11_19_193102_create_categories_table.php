3bfee99334cc180085606ca43cb8d2b3636ee23d
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-21 08:37:12 +0000

    🗃️ add slug and symbol to categories table

--- database/migrations/2022_11_19_193102_create_categories_table.php
+++ database/migrations/2022_11_19_193102_create_categories_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
+            $table->text('symbol');
             $table->string('color');
             $table->timestamps();
         });
