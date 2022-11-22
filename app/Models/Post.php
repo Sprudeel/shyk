26fcbef65f19dff561f503e71d302716d1829ba7
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-22 15:35:10 +0000

    ✨ add slug conversion

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'topic', 'status', 'category', 'content', 'author'];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function ($post) {
+            $post->slug = Str::slug($post->title);
+        });
+    }
 
 
     public function topic() {
