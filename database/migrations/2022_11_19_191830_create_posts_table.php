dfba2981bc84ebc2b7bbf2cb8c900786f631b39b
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-22 15:35:19 +0000

    🗃️ add slug to post

--- database/migrations/2022_11_19_191830_create_posts_table.php
+++ database/migrations/2022_11_19_191830_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug')->unique();
             $table->string('author');
             $table->string('topic');
             $table->string('category');
