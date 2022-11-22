40be2b5e7b3cb0ea77b4ed94ab021e597bb1e752
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-22 11:00:56 +0000

    🚀 add max:30 to post title in validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,7 +33,7 @@ class PostController extends Controller
 
 
         $request->validate([
-            'title' => 'required|unique:App\Models\Post,title',
+            'title' => 'required|unique:App\Models\Post,title|max:30',
             'topic' => 'required|exists:topics,slug',
             'status' => 'required',
             'category' => 'required|exists:categories,slug',
