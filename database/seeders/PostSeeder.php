5adc5161afc1419fd3ad6a13a40d22140f1a18a5
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-22 11:02:22 +0000

    🌱 add post seeding

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -14,6 +15,44 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('posts')->insert([
+            'title' => 'shyk - news',
+            'author' => 1,
+            'topic' => 'shyk',
+            'category' => 'questions',
+            'status' => 'public',
+            'content' => 'Lorem Ipsum',
+            'created_at' => now(),
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'Mathematik',
+            'author' => 1,
+            'topic' => 'math',
+            'category' => 'questions',
+            'status' => 'public',
+            'content' => 'Lorem Ipsum',
+            'created_at' => now(),
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'GGGGGGGGGGGGGGGGGGGGGGGGGGGGGG',
+            'author' => 1,
+            'topic' => 'shyk',
+            'category' => 'summaries',
+            'status' => 'public',
+            'content' => 'Lorem Ipsum',
+            'created_at' => now(),
+        ]);
+
+        DB::table('posts')->insert([
+            'title' => 'Mathematik Zusammenfassung Analysis',
+            'author' => 1,
+            'topic' => 'math',
+            'category' => 'summaries',
+            'status' => 'public',
+            'content' => 'Lorem Ipsum',
+            'created_at' => now(),
+        ]);
     }
 }
