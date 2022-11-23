4789e8eb89fd32bde731133ffa570cb1bcb534a3
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-23 21:47:21 +0000

    ✨ add myposts

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -16,11 +16,18 @@ use Illuminate\Support\Str;
 class TopicController extends Controller
 {
     public function index(Request $request) {
+        $myposts = [];
+
+        if($request->topic == 'my') {
+            $myposts = Auth::user() && $request->category ? Post::where(['status' => 'public', 'author' => Auth::user()->id, 'category' => $request->category])->with('author.role', 'topic', 'category', 'likes')->get() : ( Auth::user() ? Post::where(['status' => 'public', 'author' => Auth::user()->id])->with('author.role', 'topic', 'category', 'likes')->get() : null );
+        }
+
         return Inertia::render('Discover', [
-            'topic' => $request->topic ? Topic::where('slug', $request->topic)->firstOrFail() : null ,
+            'topic' => $request->topic ? Topic::where('slug', $request->topic)->first() : null,
             'topics' => Topic::all(),
             'categories' => Category::all(),
-            'posts' => $request->topic && $request->category ? Post::where(['status' => 'public', 'topic' => $request->topic, 'category' => $request->category])->with('author.role', 'topic', 'category')->get() : ( $request->topic ? Post::where(['status' => 'public', 'topic' => $request->topic])->with('author.role', 'topic', 'category')->get() : ( $request->category ? Post::where(['status' => 'public', 'category' => $request->category])->with('author.role', 'topic', 'category')->get() : Post::where('status', 'public')->with('author.role', 'topic', 'category')->get() ) ),
+            'posts' => $request->topic && $request->category ? Post::where(['status' => 'public', 'topic' => $request->topic, 'category' => $request->category])->with('author.role', 'topic', 'category', 'likes')->get() : ( $request->topic ? Post::where(['status' => 'public', 'topic' => $request->topic])->with('author.role', 'topic', 'category', 'likes')->get() : ( $request->category ? Post::where(['status' => 'public', 'category' => $request->category])->with('author.role', 'topic', 'category', 'likes')->get() : Post::where('status', 'public')->with('author.role', 'topic', 'category', 'likes')->get() ) ),
+            'myposts' => $myposts,
         ]);
     }
 }
