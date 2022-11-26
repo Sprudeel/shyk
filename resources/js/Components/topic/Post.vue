0f2473f0010acc5ba6eb967b4cb605330f8ef366
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-11-26 20:54:45 +0000

    💄 add private icon

--- resources/js/Components/topic/Post.vue
+++ resources/js/Components/topic/Post.vue
@@ -23,6 +23,23 @@ const auth = computed(() => usePage().props.value.auth);
                 <div
                     class="col-span-2 flex flex-row self-center text-xl font-bold tracking-wide"
                 >
+                    <span
+                        v-if="props.data.status == 'private'"
+                        title="Post ist privat!"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            height="24"
+                            class="mr-4"
+                        >
+                            <path fill="none" d="M0 0h24v24H0z" />
+                            <path
+                                d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zm-7 7.732V18h2v-2.268a2 2 0 1 0-2 0zM16 8V7a4 4 0 1 0-8 0v1h8z"
+                            />
+                        </svg>
+                    </span>
                     {{ props.data.title }}
                     <span
                         v-if="props.data.verified"
