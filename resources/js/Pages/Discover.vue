6f0b811ef4b3c5dab52c3ab163150441b4b408ba
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-12-04 14:57:43 +0000

    📱 responsive discover page

--- resources/js/Pages/Discover.vue
+++ resources/js/Pages/Discover.vue
@@ -21,28 +21,32 @@ const props = defineProps({
     <DefaultLayout>
         <Banner :topic="props.topic" />
 
-        <div class="mt-8 grid grid-cols-3 gap-16">
-            <div class="col-span-1 h-full justify-self-end">
-                <div>
-                    <ForumMenu
-                        :topics="props.topics"
-                        :categories="props.categories"
-                    />
+        <div class="mr-8 ml-8">
+            <div class="mt-8 gap-8 md:grid md:auto-cols-auto md:grid-flow-col">
+                <div
+                    class="h-full ph:mx-auto ph:mb-8 ph:text-center md:justify-self-end"
+                >
+                    <div>
+                        <ForumMenu
+                            :topics="props.topics"
+                            :categories="props.categories"
+                        />
+                    </div>
                 </div>
-            </div>
-            <div class="col-span-2 mb-8 w-full">
-                <Post
-                    v-for="post in props.posts.data"
-                    :key="post.id"
-                    :data="post"
-                />
-                <Post
-                    v-if="route().params.topic === 'my'"
-                    v-for="post in props.myposts.data"
-                    :key="post.id"
-                    :data="post"
-                />
-                <Pagination class="mt-6" :links="props.posts.links" />
+                <div class="w-full md:col-span-2 md:mb-8">
+                    <Post
+                        v-for="post in props.posts.data"
+                        :key="post.id"
+                        :data="post"
+                    />
+                    <Post
+                        v-if="route().params.topic === 'my'"
+                        v-for="post in props.myposts.data"
+                        :key="post.id"
+                        :data="post"
+                    />
+                    <Pagination class="m-6" :links="props.posts.links" />
+                </div>
             </div>
         </div>
     </DefaultLayout>
