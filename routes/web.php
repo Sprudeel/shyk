484802d640f88f274e71723be5b0be5e237dbc5c
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-12-04 16:14:19 +0000

    🚚 add search route

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::get('/discover/{topic?}', [TopicController::class, 'index'])->name('disco
 
 Route::post('/tmpupload', [UploadController::class, 'store'])->middleware(['auth', 'verified'])->name('tmpupload');
 Route::delete('/tmpdelete', [UploadController::class, 'destroy'])->middleware(['auth', 'verified'])->name('tmpdelete');
+Route::get('/search', [TopicController::class, 'search'])->middleware(['auth', 'verified'])->name('topic.search');
 
 Route::pattern('path', '.*');
 Route::get('local/temp/{path}', function (string $path){
