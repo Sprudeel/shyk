eaa5e0de5ffa8109d604789977ea36b5c7bf032a
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-12-04 16:14:54 +0000

    💄 add search banner

--- resources/js/Components/topic/Banner.vue
+++ resources/js/Components/topic/Banner.vue
@@ -1,8 +1,10 @@
 <script setup>
 import SmartLogo from "@/Components/svg/logo/Smart.vue";
+import QuestionLogo from "@/Components/svg/logo/Question.vue";
 
 const props = defineProps({
-    topic: Object,
+    topic: String,
+    search: String,
 });
 </script>
 
@@ -26,7 +28,25 @@ const props = defineProps({
         </div>
     </div>
 
-    <div v-if="props.topic !== null">
+    <div v-else-if="props.topic === 'search'">
+        <div
+            class="mx-auto flex w-full flex-row items-center bg-slate-200 py-4"
+        >
+            <span class="m-4 basis-1/3">
+                <QuestionLogo class="mx-auto h-32" />
+            </span>
+            <div class="mr-12 basis-2/3">
+                <h2 class="font-extrabold ph:text-lg lph:text-2xl md:text-4xl">
+                    " {{ props.search }} "
+                </h2>
+                <p class="ph:hidden lph:block lph:text-lg md:text-xl">
+                    wurde gesucht...
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div v-else>
         <div
             :style="
                 'background: linear-gradient(120deg, ' +
