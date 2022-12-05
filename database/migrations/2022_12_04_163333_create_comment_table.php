a170fcf7f7ac5377e02e82bfd0db740658d5480a
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-12-05 08:50:24 +0000

    🗃️ add comments db table

---
+++ database/migrations/2022_12_04_163333_create_comment_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id');
+            $table->string('comment_on');
+            $table->string('comment_parent');
+            $table->integer('commenter');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+};
