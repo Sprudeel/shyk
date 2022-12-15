3dbe2dd78281aa91896ba24acd308b61fa970021
Léonard Schafer <79663902+Sprudeel@users.noreply.github.com>
2022-12-15 14:28:35 +0000

    💄 change likes design

--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -204,7 +204,7 @@ function convert(value) {
                     </div>
                 </div>
             </div>
-            <div class="mt-2 ml-4 h-fit ph:flex ph:justify-around sm:flex-col">
+            <div class="mt-12 ml-2 h-fit ph:flex ph:justify-around sm:flex-col">
                 <span class="mb-4 flex flex-row" v-if="auth.user">
                     <Link
                         v-if="
@@ -260,7 +260,7 @@ function convert(value) {
                     as="button"
                 >
                     <span
-                        class="mb-8 flex w-fit cursor-pointer flex-row space-x-2 rounded-lg bg-slate-200 py-2 px-4 hover:bg-slate-300"
+                        class="mb-4 flex w-fit cursor-pointer flex-row space-x-2 rounded-lg py-2"
                         v-if="
                             !liked && auth.user && props.post.status == 'public'
                         "
@@ -276,10 +276,10 @@ function convert(value) {
                                 d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z"
                             />
                         </svg>
-                        <span>Gefällt mir</span>
+                        <span>{{ props.likes.length }}</span>
                     </span>
                     <span
-                        class="mb-8 flex w-fit cursor-pointer flex-row space-x-2 rounded-lg bg-red-200 py-2 px-4 hover:bg-red-300"
+                        class="mb-4 flex w-fit cursor-pointer flex-row space-x-2 rounded-lg py-2"
                         v-if="
                             liked && auth.user && props.post.status == 'public'
                         "
@@ -296,7 +296,7 @@ function convert(value) {
                                 d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228z"
                             />
                         </svg>
-                        <span>Gefällt mir</span>
+                        <span>{{ props.likes.length }}</span>
                     </span>
                 </Link>
 
